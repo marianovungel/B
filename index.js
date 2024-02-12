827c2c46c7998d4677474316067e9078fba4e504
marianovungel <vungem88@gmail.com>
2024-02-12 11:53:34 -0300

    new

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
     res.header("Access-Control-Allow-Methods", 'GET,POST','PUT','DELETE');
     app.use(cors())
     next();
