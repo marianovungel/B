b1b3d7cf58172430b86a97b689e87016d85a24c4
marianovungel <vungem88@gmail.com>
2024-02-12 12:29:27 -0300

    info 3

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use((req, res, next)=>{
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/atualadm");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,POST','PUT','DELETE');
     app.use(cors())
     next();
@@ -22,7 +22,7 @@ app.use((req, res, next)=>{
 app.use(cors())
 
 app.get("/", (req, res)=>{
-    res.json({data: "firebase 2"})
+    res.json({data: "firebase 3"})
 
 })
 
